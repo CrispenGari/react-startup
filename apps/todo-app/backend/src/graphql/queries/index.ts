ec2523bbc4339b50a3ffb3fc6a10f9887d2f47da
CrispenGari <Crispengari@gmail.com>
2021-07-01 00:54:01 +0200

    rest-done-and-query-type-

--- apps/todo-app/backend/src/graphql/queries/index.ts
+++ apps/todo-app/backend/src/graphql/queries/index.ts
@@ -1,12 +1,28 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
+import { TodoTypeWithUsername } from "../types";
+
+import { Todos } from "../../db";
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQuery",
   description: "This is the root query type for users and todos.",
   fields: {
-    message: {
-      type: GraphQLString,
-      resolve: () => "Hello, World.",
+    todos: {
+      description: "All todos of the user.",
+      type: GraphQLList(TodoTypeWithUsername),
+      args: {
+        username: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: async (parent, args) => {
+        return await Todos.find({ username: args.username });
+      },
     },
   },
 });
--- apps/todo-app/backend/src/graphql/types/index.ts
+++ apps/todo-app/backend/src/graphql/types/index.ts
@@ -1,3 +1,51 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLNonNull,
+} from "graphql";
 
+export const TodoTypeWithUsername = new GraphQLObjectType({
+  name: "TodoTypeWithUsername",
+  description:
+    "This is a todo type that contains the username of the publisher.",
+  fields: () => ({
+    username: {
+      type: GraphQLNonNull(GraphQLString),
+    },
+    _id: {
+      type: GraphQLNonNull(GraphQLString),
+    },
+    todo: {
+      type: TodoType,
+    },
+  }),
+});
+const TodoType = new GraphQLObjectType({
+  name: "TodoType",
+  description: "This is the object type.",
+  fields: () => ({
+    completed: { type: GraphQLNonNull(GraphQLBoolean) },
+    _id: {
+      type: GraphQLNonNull(GraphQLString),
+    },
+    title: {
+      type: GraphQLNonNull(GraphQLString),
+    },
+  }),
+});
 
+/*
 
+ {
+        "_id": "60dce2d4b94a281ffc15d57b",
+        "username": "crispen",
+        "todo": {
+            "completed": false,
+            "_id": "60dce2d4b94a281ffc15d57c",
+            "title": "Cooking from monday to tuesday."
+        },
+        "__v": 0
+    }
+]
+*/
